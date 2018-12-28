6db98bc931c81c5c6e6cea9cad62ace5f3ebbadb
thZero.com <development@thzero.com>
2018-12-28 10:27:54 -0600

    Converted to BaseStartupExtension

--- HealtchchecksInstrumentationStartupExtension.cs
+++ HealtchchecksInstrumentationStartupExtension.cs
@@ -22,100 +22,26 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace thZero.AspNetCore
 {
-    public class HealtchchecksInstrumentationStartupExtension : IStartupExtension
+    public class HealtchchecksInstrumentationStartupExtension : BaseStartupExtension
     {
         #region Public Methods
-        public virtual void ConfigurePost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
+        public override void ConfigureInitializePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
         {
-        }
+            base.ConfigureInitializePost(app, env, loggerFactory, svp);
 
-        public virtual void ConfigurePre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializePost(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
-        {
             ConfigureInitializeHealthChecks(app);
         }
 
-        public virtual void ConfigureInitializePre(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IServiceProvider svp)
+        public override void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfiguration configuration)
         {
-        }
+            base.ConfigureServicesInitializeMvcPre(services, configuration);
 
-        public virtual void ConfigureInitializeFinalPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeFinalPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeRoutesBuilderPost(IRouteBuilder routes)
-        {
-        }
-
-        public virtual void ConfigureInitializeRoutesBuilderPre(IRouteBuilder routes)
-        {
-        }
-
-        public virtual void ConfigureInitializeSsl(IApplicationBuilder app)
-        {
-        }
-
-        public virtual void ConfigureInitializeStaticPost(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureInitializeStaticPre(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider svp)
-        {
-        }
-
-        public virtual void ConfigureServicesPost(IServiceCollection services, IConfiguration configuration)
-        {
-        }
-
-        public virtual void ConfigureServicesPre(IServiceCollection services, IConfiguration configuration)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPost(IMvcCoreBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcBuilderPre(IMvcCoreBuilder builder)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcOptionsPost(MvcOptions options)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcOptionsPre(MvcOptions options)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfiguration configuration)
-        {
-        }
-
-        public virtual void ConfigureServicesInitializeMvcPre(IServiceCollection services, IConfiguration configuration)
-        {
             ConfigureServicesHealthChecks(services.AddHealthChecks());
         }
         #endregion
@@ -123,7 +49,7 @@ namespace thZero.AspNetCore
         #region Protected Methods
         protected virtual void ConfigureInitializeHealthChecks(IApplicationBuilder app)
         {
-            UseHealthCheck(app, Route);
+            UseHealthCheck(app);
         }
 
         protected virtual void ConfigureServicesHealthChecks(IHealthChecksBuilder builder)
@@ -142,7 +68,10 @@ namespace thZero.AspNetCore
             if (string.IsNullOrEmpty(route))
                 throw new Exception("Invalid health check route.");
 
-            app.UseHealthChecks(route, options);
+            if (options != null)
+                app.UseHealthChecks(route, options);
+            else
+                app.UseHealthChecks(route);
         }
         #endregion
 
